17d880f3f2948e4ea7af3f424bf64e94d5f45a6b
PRANAV-SKCET <pranavarulprakash@gmail.com>
2024-08-28 21:05:56 +0530

    commit

--- inter_department_communication/src/components/projectManager/projectManagerDashboard.jsx
+++ inter_department_communication/src/components/projectManager/projectManagerDashboard.jsx
@@ -1,7 +1,10 @@
+import ProjectManagerNavbar from "./projectManagerNavbar";
+
 export default function ProjectManagerDashboard()
 {
     return(
         <div>
+            <ProjectManagerNavbar/>
             Manager Dashboard
         </div>
     );
